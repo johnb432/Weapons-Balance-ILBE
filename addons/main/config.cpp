c030d99d9608a8dacc3e008ee82d2e57acedd41b
johnb432 <58661205+johnb432@users.noreply.github.com>
2022-02-22 18:14:00 +0100

    1.0.0.2

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -10,25 +10,9 @@ class CfgPatches {
             "tfw_radios_ilbe"
         };
         author = "johnb43";
+        authors[] = {"johnb43"};
         url = "https://github.com/johnb432/Weapons-Balance-ILBE";
         VERSION_CONFIG;
-    };
-};
-
-class CfgMods {
-    class PREFIX {
-        name = "Weapons Balance - ILBE Rewrite";
-        author = "johnb43";
-        tooltipOwned = "Weapons Balance - ILBE Rewrite";
-        hideName = 0;
-        hidePicture = 0;
-        actionName = "Github";
-        action = "https://github.com/johnb432/Weapons-Balance-ILBE";
-        description = "A collection of balancing mods made by johnb43.";
-        overview = "A collection of balancing mods made by johnb43.";
-        picture = "\z\wb_ilbe\addons\main\ui\logo_weapons_balance.paa"; // http://getdrawings.com/get-drawing#gun-drawing-in-pencil-27.jpg, http://getdrawings.com/get-drawing#gun-drawing-in-pencil-17.png
-        logo = "\z\wb_ilbe\addons\main\ui\logo_weapons_balance.paa";
-        overviewPicture = "\z\wb_ilbe\addons\main\ui\logo_weapons_balance.paa";
     };
 };
 
